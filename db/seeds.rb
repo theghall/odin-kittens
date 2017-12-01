f5cc06a1eec33b4c41f269fc3eade5df108435ae
Gary Hall <guategary@gmail.com>
2017-12-01 16:11:03 -0500

    Update seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Kitten.create(name: 'Fluffy', age: 5, cuteness: 8, softness: 10)
+Kitten.create(name: 'Hardnose', age: 7, cuteness: 3, softness: 1)
+Kitten.create(name: 'Spot', age: 3, cuteness: 1, softness: 6)
+Kitten.create(name: 'Fancy', age: 8, cuteness: 9, softness: 7)
+Kitten.create(name: 'Princess', age: 4, cuteness: 10, softness: 10)
