a2c602597c1595dde6b0698f5cc3f55f081e9b4c
Gary Hall <guategary@gmail.com>
2017-12-01 17:29:01 -0500

    Add json response for index and show

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -5,11 +5,22 @@ class KittensController < ApplicationController
   # GET /kittens.json
   def index
     @kittens = Kitten.all
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @kittens }
+    end
   end
 
   # GET /kittens/1
   # GET /kittens/1.json
   def show
+    @kitten = Kitten.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @kitten }
+    end
   end
 
   # GET /kittens/new
--- app/views/kittens/_kitten.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! kitten, :id, :created_at, :updated_at
-json.url kitten_url(kitten, format: :json)
